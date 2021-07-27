1a414751dafc6c40dece6cd2a890f4878c55bbdf
Kseniya St <kseniya7991@gmail.com>
2021-07-27 20:21:22 +0300

    add user mongoose Schema

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Введите корректный email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+})
+
+module.exports = mongoose.model('user', userSchema);
