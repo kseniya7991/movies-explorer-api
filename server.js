8c8d4ab1e70a63ae2011945a92b10701aca11ead
Kseniya St <kseniya7991@gmail.com>
2021-07-31 12:53:09 +0300

    add DB address to .env

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
+const { DB, NODE_ENV } = process.env;
+
 const { PORT = 3000 } = process.env;
 
 // подключение к серверу Mongo
 async function start() {
   try {
     app.listen(PORT, () => `Server is running on port ${PORT}`);
-    await mongoose.connect('mongodb://localhost:27017/moviesdb', {
+    await mongoose.connect(NODE_ENV === 'production' ? DB : 'mongodb://localhost:27017/moviesdb', {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
