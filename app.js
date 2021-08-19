3a506ef1baf1a3008053f178a5a4fefb22fdf8c0
Kseniya St <kseniya7991@gmail.com>
2021-08-19 13:28:21 +0300

    disabled CORS temporary

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  const requestHeaders = req.headers['access-contorl-request-headers'];
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
